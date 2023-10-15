0ae1a0df3c417f8a8c9d43439bcee8dc6e0ac6ea
LAPTOP-NVUJBTUF\tomiz <tomizawa@efit.co.jp>
2023-10-15 15:15:46 +0900

    フォーム送り主名前、電話番号修正

--- contact/contact-confirm.php
+++ contact/contact-confirm.php
@@ -67,142 +67,153 @@ if(empty ($_POST['c_name']) or empty ($_POST['c_address']) or empty ($_POST['c_c
 
 <!DOCTYPE html>
 <html lang="jp">
-    <head>
-        <!-- Meta -->
-        <meta charset="utf-8">
-<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <title>TCW トミザワ・クリエイティブ ワークス お問い合わせ確認ページ</title>
-        <meta name="description" content="TCW（トミザワ クリエイティブ ワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
-        <meta name="keyword" content="tcw,トミザワクリエイティブワークス,web制作,ホームページ作成,webデザイン,webコーディング,保守運用,フロントエンドエンジニア,フリーランス">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!-- CSS定義 -->
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <!--        <link rel="stylesheet" type="text/css" href="utility/u-color.css">-->
-        <link rel="stylesheet" href="../css/fundations/reset.css">
-        <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:400,500,700&display=swap" rel="stylesheet">
 
-        <!--        ↓ドロワーメニュー-->
-        <!-- JS定義 -->
-        <script src="../js/jquery-3.5.0.min.js"></script>
-        <!-- drawer.css -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
-        <!-- jquery &amp; iScroll -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
-        <!-- drawer.js -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
-        <!--       vuw.js-->
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vue2-smooth-scroll@1.5.0/dist/vue-smooth-scroll.min.js"></script>
-       
-        <!--    ogp-->
-        <meta property="og:image" content="https://t-creative-works.com/image/tcw_ogp.jpg">
-        <meta property="og:type" content="website">
-        <meta property="og:title" content="TCW トミザワ・クリエイティブワークス">
-        <meta property="og:url" content="https://t-creative-works.com/">
-        <meta property="og:site_name" content="TCW トミザワ・クリエイティブワークス">
-        <meta property="og:description" content="TCW（トミザワ・クリエイティブワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
-        <meta property="og:locale" content="ja_JP">
-        <meta property="”fb:app_id”" content="”App-ID(15文字の半角数字)”">
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>TCW トミザワ・クリエイティブ ワークス お問い合わせ確認ページ</title>
+    <meta name="description"
+        content="TCW（トミザワ クリエイティブ ワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
+    <meta name="keyword" content="tcw,トミザワクリエイティブワークス,web制作,ホームページ作成,webデザイン,webコーディング,保守運用,フロントエンドエンジニア,フリーランス">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- CSS定義 -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <!--        <link rel="stylesheet" type="text/css" href="utility/u-color.css">-->
+    <link rel="stylesheet" href="../css/fundations/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:400,500,700&display=swap" rel="stylesheet">
 
-        <!--    favicon-->
-        <link rel="shortcut icon" href="../image/favicon.gif" type="image/gif">
-        <link rel="icon" href="../image/favicon.gif" type="image/gif">
-        <!--    スマホ用アイコン画像-->
-        <link rel="apple-touch-icon-precomposed" href="../image/touch-icon.png">
-        <!--    電話番号やメールアドレスの変換設定防止タグ-->
-        <meta name="format-detection" content="email=no,telephone=no,address=no">
-        
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6B8SHB1V7D"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+    <!--        ↓ドロワーメニュー-->
+    <!-- JS定義 -->
+    <script src="../js/jquery-3.5.0.min.js"></script>
+    <!-- drawer.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
+    <!-- jquery &amp; iScroll -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
+    <!-- drawer.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
+    <!--       vuw.js-->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue2-smooth-scroll@1.5.0/dist/vue-smooth-scroll.min.js"></script>
 
-            gtag('config', 'G-6B8SHB1V7D');
-        </script>
-    </head>
+    <!--    ogp-->
+    <meta property="og:image" content="https://t-creative-works.com/image/tcw_ogp.jpg">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="TCW トミザワ・クリエイティブワークス">
+    <meta property="og:url" content="https://t-creative-works.com/">
+    <meta property="og:site_name" content="TCW トミザワ・クリエイティブワークス">
+    <meta property="og:description"
+        content="TCW（トミザワ・クリエイティブワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="”fb:app_id”" content="”App-ID(15文字の半角数字)”">
+
+    <!--    favicon-->
+    <link rel="shortcut icon" href="../image/favicon.gif" type="image/gif">
+    <link rel="icon" href="../image/favicon.gif" type="image/gif">
+    <!--    スマホ用アイコン画像-->
+    <link rel="apple-touch-icon-precomposed" href="../image/touch-icon.png">
+    <!--    電話番号やメールアドレスの変換設定防止タグ-->
+    <meta name="format-detection" content="email=no,telephone=no,address=no">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6B8SHB1V7D"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-6B8SHB1V7D');
+    </script>
+</head>
 
 
-    <body class="drawer drawer--top p-p_under">
-        <?php include('../header.php');?>
-        
-        <main class="p-p_portfolio p-p_works p-p_contact">
-            <div class="f-wrap">
-                <div class="f-wrap-inner">
-                    <div id="p-contact" class="p_confirm">
-                        <h2 class="c-head_2">Contact</h2>
-                        <div class="p-c_sub_ttl">
-                            <ul>
-                                <li>入力</li>
-                                <li class="p-c_s_correct">確認</li>
-                                <li>完了</li>
-                            </ul>
+<body class="drawer drawer--top p-p_under">
+    <?php include('../header.php');?>
+
+    <main class="p-p_portfolio p-p_works p-p_contact">
+        <div class="f-wrap">
+            <div class="f-wrap-inner">
+                <div id="p-contact" class="p_confirm">
+                    <h2 class="c-head_2">Contact</h2>
+                    <div class="p-c_sub_ttl">
+                        <ul>
+                            <li>入力</li>
+                            <li class="p-c_s_correct">確認</li>
+                            <li>完了</li>
+                        </ul>
+                    </div>
+                    <form action="contact-last.php" method="post">
+
+                        <div class="form-block">
+                            <div class="form-topic">
+                                お名前（<span>必須</span>）
+                            </div>
+                            <div class="form-input">
+                                <?php print $c_name;?><?php print $c_name_emp;?>
+                            </div>
                         </div>
-                        <form action="contact-last.php" method="post">
-                           
-                            <div class="form-block">
-                                <div class="form-topic">
-                                    お名前（<span>必須</span>）
-                                </div>
-                                <div class="form-input">
-                                    <?php print $c_name;?><?php print $c_name_emp;?>
-                                </div>
+                        <div class="form-block">
+                            <div class="form-topic">
+                                会社名
                             </div>
-                            <div class="form-block">
-                                <div class="form-topic">
-                                    会社名
-                                </div>
-                                <div class="form-input">
-                                    <?php print $c_company?><?php print $c_company_emp;?>
-                                </div>
+                            <div class="form-input">
+                                <?php print $c_company?><?php print $c_company_emp;?>
                             </div>
-                            
-                            <div class="form-block">
-                                <div class="form-topic">
-                                    メールアドレス（<span>必須</span>）
-                                </div>
-                                <div class="form-input">
-                                    <?php print $c_address?><?php print $c_address_emp;?>
-                                </div>
-                            </div>
+                        </div>
 
-                            <div class="form-block">
-                                <div class="form-topic">
-                                    お問い合わせ内容（<span>必須</span>）
-                                </div>
-                                <div class="form-input">
-                                    <?php print $c_content;?><?php print $c_content_emp;?>
-                                </div>
+                        <div class="form-block">
+                            <div class="form-topic">
+                                メールアドレス（<span>必須</span>）
                             </div>
-                            
-                            <!--                    ↓送信用のデータを変数で格納-->
-                            <input type="hidden" value="<?php echo $c_name; ?>" name="c_name"/>
-                            <input type="hidden" value="<?php echo $c_company; ?>" name="c_company"/>
-                            <input type="hidden" value="<?php echo $c_address; ?>" name="c_address"/>
-                            <input type="hidden" value="<?php echo $c_content; ?>" name="c_content"/>
-                            <div class="result-block<?PHP echo $result_block_one ?>">
-                            <input type="button" onclick="history.back()" value="戻る" class="result-input result-input01">
-                            <input type="button" onclick="submit();" value="送信" class="result-input<?PHP echo $result_input02 ?>" name="sub">
+                            <div class="form-input">
+                                <?php print $c_address?><?php print $c_address_emp;?>
                             </div>
-                            
-                        </form>
-                        <div class="p-c_address">
-                            <p>※もし送信されない場合は、下記の連絡先に直接お問い合わせください。</p>
-                            <div class="icons">
-                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-mail.png" alt=""><a href="mailto:m.tomizawa@t-creative-works.com">tomizawa@t-creative-works.com</a></span></div>
-                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-tell.png" alt=""><a href="tel:+81-50-3199-1487">050-3199-1487</a></span></div>
-<!--                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-twitter.png" alt=""><a href="https://twitter.com/tcw_job" target="_blank">Twitter</a></span></div>-->
+                        </div>
+
+                        <div class="form-block">
+                            <div class="form-topic">
+                                お問い合わせ内容（<span>必須</span>）
                             </div>
+                            <div class="form-input">
+                                <?php print $c_content;?><?php print $c_content_emp;?>
+                            </div>
+                        </div>
+
+                        <!--                    ↓送信用のデータを変数で格納-->
+                        <input type="hidden" value="<?php echo $c_name; ?>" name="c_name" />
+                        <input type="hidden" value="<?php echo $c_company; ?>" name="c_company" />
+                        <input type="hidden" value="<?php echo $c_address; ?>" name="c_address" />
+                        <input type="hidden" value="<?php echo $c_content; ?>" name="c_content" />
+                        <div class="result-block<?PHP echo $result_block_one ?>">
+                            <input type="button" onclick="history.back()" value="戻る"
+                                class="result-input result-input01">
+                            <input type="button" onclick="submit();" value="送信"
+                                class="result-input<?PHP echo $result_input02 ?>" name="sub">
+                        </div>
+
+                    </form>
+                    <div class="p-c_address">
+                        <p>※もし送信されない場合は、下記の連絡先に直接お問い合わせください。</p>
+                        <div class="icons">
+                            <div class="icon-block"><span class="c_icon"><img src="../image/icon-mail.png" alt=""><a
+                                        href="mailto:m.tomizawa@t-creative-works.com">tomizawa@t-creative-works.com</a></span>
+                            </div>
+                            <!-- <div class="icon-block"><span class="c_icon"><img src="../image/icon-tell.png" alt=""><a
+                                        href="tel:+81-50-3199-1487">050-3199-1487</a></span></div> -->
+                            <!--                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-twitter.png" alt=""><a href="https://twitter.com/tcw_job" target="_blank">Twitter</a></span></div>-->
                         </div>
                     </div>
-                  
-                    
                 </div>
+
+
             </div>
-        </main>
-        <?php include('../footer.php');?>
+        </div>
+    </main>
+    <?php include('../footer.php');?>
 
 
 </html>
--- contact/contact-last.php
+++ contact/contact-last.php
@@ -25,12 +25,12 @@ if(isset ($_POST['c_name']) or isset ($_POST['c_address']) or isset ($_POST['c_c
     $mail = "以下の内容が送信されました。\n\n"."【お名前】\n".$c_name."\n\n"."【会社名】\n".$c_company."\n\n"."【メールアドレス】\n".$c_address."\n\n"."【お問い合わせ内容】\n".$c_content."\n\n";
 
     $mail_to = "tomizawa@t-creative-works.com";//送信先メールアドレス
-    $mail_subject = "Citationメールフォーム\n".$c_name."より送信されました";//メールの件名
+    $mail_subject = "TCWメールフォーム\n".$c_name."より送信されました";//メールの件名
     $mail_body = $mail;//メールの本文
     $mail_header = "from:".$c_address;//フォームで入力されたメールアドレスを送信元として表示する
 
     //↓$returnPathは送信先からエラーなどがあった際に通知されるアドレス
-    $returnPath = '-f'.'m.tomizawa821@gmail.com';
+    $returnPath = '-f'.'tomizawa@t-creative-works.com';
     $mailsend = mb_send_mail($mail_to, $mail_subject, $mail_body, $mail_header, $returnPath);
 }
 
@@ -40,81 +40,87 @@ if(isset ($_POST['c_name']) or isset ($_POST['c_address']) or isset ($_POST['c_c
 
 <!DOCTYPE html>
 <html lang="jp">
-    <head>
-        <!-- Meta -->
-        <meta charset="utf-8">
-<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <title>TCW トミザワ・クリエイティブ ワークス お問い合わせ完了ページ</title>
-        <meta name="description" content="TCW（トミザワ クリエイティブ ワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
-        <meta name="keyword" content="tcw,トミザワクリエイティブワークス,web制作,ホームページ作成,webデザイン,webコーディング,保守運用,フロントエンドエンジニア,フリーランス">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!-- CSS定義 -->
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <!--        <link rel="stylesheet" type="text/css" href="utility/u-color.css">-->
-        <link rel="stylesheet" href="../css/fundations/reset.css">
-        <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:400,500,700&display=swap" rel="stylesheet">
 
-        <!--        ↓ドロワーメニュー-->
-        <!-- JS定義 -->
-        <script src="../js/jquery-3.5.0.min.js"></script>
-        <!-- drawer.css -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
-        <!-- jquery &amp; iScroll -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
-        <!-- drawer.js -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
-        <!--       vuw.js-->
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vue2-smooth-scroll@1.5.0/dist/vue-smooth-scroll.min.js"></script>
-       
-        <!--    ogp-->
-        <meta property="og:image" content="https://t-creative-works.com/image/tcw_ogp.jpg">
-        <meta property="og:type" content="website">
-        <meta property="og:title" content="TCW トミザワ・クリエイティブワークス">
-        <meta property="og:url" content="https://t-creative-works.com/">
-        <meta property="og:site_name" content="TCW トミザワ・クリエイティブワークス">
-        <meta property="og:description" content="TCW（トミザワ・クリエイティブワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
-        <meta property="og:locale" content="ja_JP">
-        <meta property="”fb:app_id”" content="”App-ID(15文字の半角数字)”">
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>TCW トミザワ・クリエイティブ ワークス お問い合わせ完了ページ</title>
+    <meta name="description"
+        content="TCW（トミザワ クリエイティブ ワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
+    <meta name="keyword" content="tcw,トミザワクリエイティブワークス,web制作,ホームページ作成,webデザイン,webコーディング,保守運用,フロントエンドエンジニア,フリーランス">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- CSS定義 -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <!--        <link rel="stylesheet" type="text/css" href="utility/u-color.css">-->
+    <link rel="stylesheet" href="../css/fundations/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP:400,500,700&display=swap" rel="stylesheet">
 
-        <!--    favicon-->
-        <link rel="shortcut icon" href="../image/favicon.gif" type="image/gif">
-        <link rel="icon" href="../image/favicon.gif" type="image/gif">
-        <!--    スマホ用アイコン画像-->
-        <link rel="apple-touch-icon-precomposed" href="../image/touch-icon.png">
-        <!--    電話番号やメールアドレスの変換設定防止タグ-->
-        <meta name="format-detection" content="email=no,telephone=no,address=no">
-        
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6B8SHB1V7D"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+    <!--        ↓ドロワーメニュー-->
+    <!-- JS定義 -->
+    <script src="../js/jquery-3.5.0.min.js"></script>
+    <!-- drawer.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
+    <!-- jquery &amp; iScroll -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
+    <!-- drawer.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
+    <!--       vuw.js-->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue2-smooth-scroll@1.5.0/dist/vue-smooth-scroll.min.js"></script>
 
-            gtag('config', 'G-6B8SHB1V7D');
-        </script>
-    </head>
+    <!--    ogp-->
+    <meta property="og:image" content="https://t-creative-works.com/image/tcw_ogp.jpg">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="TCW トミザワ・クリエイティブワークス">
+    <meta property="og:url" content="https://t-creative-works.com/">
+    <meta property="og:site_name" content="TCW トミザワ・クリエイティブワークス">
+    <meta property="og:description"
+        content="TCW（トミザワ・クリエイティブワークス）は、webサイト、ホームページの制作を随時承っております。個人で事業を営むクライアント様、一部制作をご依頼したい制作会社様からの業務等もご対応致します。フリーランスとして柔軟なご対応が可能ですので、お気軽にご連絡ください。">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="”fb:app_id”" content="”App-ID(15文字の半角数字)”">
+
+    <!--    favicon-->
+    <link rel="shortcut icon" href="../image/favicon.gif" type="image/gif">
+    <link rel="icon" href="../image/favicon.gif" type="image/gif">
+    <!--    スマホ用アイコン画像-->
+    <link rel="apple-touch-icon-precomposed" href="../image/touch-icon.png">
+    <!--    電話番号やメールアドレスの変換設定防止タグ-->
+    <meta name="format-detection" content="email=no,telephone=no,address=no">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6B8SHB1V7D"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-6B8SHB1V7D');
+    </script>
+</head>
 
 
-    <body class="drawer drawer--top p-p_under">
-        <?php include('../header.php');?>
-        
-        <main class="p-p_portfolio p-p_works p-p_contact">
-            <div class="f-wrap">
-                <div class="f-wrap-inner">
-                    <div id="p-contact" class="conirm-last">
-                        <h2 class="c-head_2">Contact</h2>
-                        <div class="p-c_sub_ttl">
-                            <ul>
-                                <li>入力</li>
-                                <li>確認</li>
-                                <li class="p-c_s_correct">完了</li>
-                            </ul>
-                        </div>
-                        <div class="contact-finish">
-                            <?php
+<body class="drawer drawer--top p-p_under">
+    <?php include('../header.php');?>
+
+    <main class="p-p_portfolio p-p_works p-p_contact">
+        <div class="f-wrap">
+            <div class="f-wrap-inner">
+                <div id="p-contact" class="conirm-last">
+                    <h2 class="c-head_2">Contact</h2>
+                    <div class="p-c_sub_ttl">
+                        <ul>
+                            <li>入力</li>
+                            <li>確認</li>
+                            <li class="p-c_s_correct">完了</li>
+                        </ul>
+                    </div>
+                    <div class="contact-finish">
+                        <?php
                             if($mailsend == true) {
                                 echo '<p class="c-f_true">メールを送信しました。折り返しご連絡差し上げますので、もうしばらくお待ちください。</p>';
                                 echo '<form method="post" action="contact.php">';
@@ -127,9 +133,9 @@ if(isset ($_POST['c_name']) or isset ($_POST['c_address']) or isset ($_POST['c_c
                                 echo '</form>';
                             }
                             ?>
-                        </div>
+                    </div>
 
-<!--
+                    <!--
                         <form action="contact-confirm.php" method="post" enctype="multipart/form-data">
                             <div class="form-block">
                                 <div class="form-topic">
@@ -168,21 +174,24 @@ if(isset ($_POST['c_name']) or isset ($_POST['c_address']) or isset ($_POST['c_c
                             </div>
                         </form>
 -->
-                        <div class="p-c_address">
-                            <p>※もし送信されない場合は、下記の連絡先に直接お問い合わせください。</p>
-                            <div class="icons">
-                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-mail.png" alt=""><a href="mailto:m.tomizawa@t-creative-works.com">tomizawa@t-creative-works.com</a></span></div>
-                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-tell.png" alt=""><a href="tel:+81-50-3199-1487">050-3199-1487</a></span></div>
-<!--                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-twitter.png" alt=""><a href="https://twitter.com/tcw_job" target="_blank">Twitter</a></span></div>-->
+                    <div class="p-c_address">
+                        <p>※もし送信されない場合は、下記の連絡先に直接お問い合わせください。</p>
+                        <div class="icons">
+                            <div class="icon-block"><span class="c_icon"><img src="../image/icon-mail.png" alt=""><a
+                                        href="mailto:m.tomizawa@t-creative-works.com">tomizawa@t-creative-works.com</a></span>
                             </div>
+                            <!-- <div class="icon-block"><span class="c_icon"><img src="../image/icon-tell.png" alt=""><a
+                                        href="tel:+81-50-3199-1487">050-3199-1487</a></span></div> -->
+                            <!--                                <div class="icon-block"><span class="c_icon"><img src="../image/icon-twitter.png" alt=""><a href="https://twitter.com/tcw_job" target="_blank">Twitter</a></span></div>-->
                         </div>
                     </div>
-                  
-                    
                 </div>
+
+
             </div>
-        </main>
-        <?php include('../footer.php');?>
+        </div>
+    </main>
+    <?php include('../footer.php');?>
 
 
 </html>
